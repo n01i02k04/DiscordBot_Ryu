2b2c9e37f0ff8d698d8f2cca292d813963d147a0
n01i02k04 <80088824+n01i02k04@users.noreply.github.com>
2022-06-23 17:57:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-from discord.ext import commands
+from Discord_Bot.Bot.start_bot import client
 from settings_local import token
 
-client = commands.Bot(command_prefix='!')
-client.remove_command('help')
-
-for filename in os.listdir('./Discord_Bot'):
-    if filename.endswith('.py'):
-        client.load_extension(f'Discord_Bot.{filename[:-3]}')
-
-client.run(token)
+if __name__ == '__main__':
+    client.run(token)
